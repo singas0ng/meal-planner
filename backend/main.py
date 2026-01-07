42b896581d38ec34c448c58a6309190c0a6355a7
song <smexykrngirl@gmail.com>
2026-01-07 15:13:47 +0900

    connect front to back with CORS

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-@app.get("/")
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/api/message")
 def read_root():
-    return {"message": "Hello from FastAPI backend!"}
+    return {"message": "connected!!!!!!" }
